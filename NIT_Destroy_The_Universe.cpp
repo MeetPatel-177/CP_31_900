7a6ce77e5730a9712fdcabb570daf8e5f8ff0a3d
MeetPatel-177 <meetpatel47477@gmail.com>
2025-03-16 12:33:04 +0530

    NIT Destroy The Universe

--- NIT_Destroy_The_Universe.cpp
+++ NIT_Destroy_The_Universe.cpp
@@ -10,9 +10,17 @@ int main() {
         vector<int>arr(n);
         for(int i=0; i<n; i++) cin >> arr[i];
         int i=0, j=n-1;
+        //first we will check both the corner part
         while(i < n && arr[i] == 0) i++;
         while(j >= 0 && arr[j] == 0) j--;
+        //now if i > j that means whole array has already all zeros
         if(i > j) cout << 0 << endl;
+        //if not above then we will traverse in remaining part of array that is from i to j
+        //if we encounter a zero in between of traversing then we will make whole array from
+        //i to j as the smallest element that does not exist in between and in second ope
+        //we will again select the same subsegment and make whole array zero
+        //if we can't encounter any zero in between that means in between all elements
+        //are non zero so we just select the whole subsegment and make them zero
         else {
             int found = 0;
             while(i <= j) {
